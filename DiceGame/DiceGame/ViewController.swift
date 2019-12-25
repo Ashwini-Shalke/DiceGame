dc68eb32d7c73a1415bdbd8e4d37a5fe884cd80b
Ashwini shalke <ashwinishalke050@gmail.com>
2019-12-25 17:52:52 +0530

    Intial commit

---
+++ DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by Ashwini shalke on 25/12/19.
+//  Copyright © 2019 Ashwini Shalke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
